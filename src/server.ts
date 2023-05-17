7b3eb7bbd6cdf172ff6ab8483612d04701596bcb
DevFC-M <felipech3344@gmail.com>
2023-05-17 00:00:40 -0300

    Finalizando a configuração inicial para o receber o codigo do back-end

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import fastify from "fastify";
 
+import { PrismaClient } from "@prisma/client";
+
 const app = fastify();
+const prisma = new PrismaClient();
 
-
+app.get("/users", async () => {
+  const users = await prisma.user.findMany();
+});
 
 app
   .listen({
